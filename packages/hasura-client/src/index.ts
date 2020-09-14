cdc85f02f6794db97a72b6ab941850a63498f7fb
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-09-14 16:00:54 +0200

    refactor: remove transient hasura client
    affects: @platyplus/hasura-client

--- packages/hasura-client/src/index.ts
+++ packages/hasura-client/src/index.ts
@@ -4,11 +4,16 @@ import { DocumentNode, print } from 'graphql'
 import { GraphQLClient } from 'graphql-request'
 import { Variables } from 'graphql-request/dist/types'
 
-class Client extends GraphQLClient {
+const DEFAULT_HASURA_ENDPOINT =
+  process.env.HASURA_ENDPOINT || 'http://graphql-engine:8080/v1/graphql'
+const DEFAULT_ADMIN_SECRET = process.env.HASURA_GRAPHQL_ADMIN_SECRET
+/**
+ * * Simple graphql-request wrapper. Pre-configured Hasura parameters (endpoint and admin-secret)
+ */
+export class Client extends GraphQLClient {
   constructor(
-    endpoint = process.env.HASURA_ENDPOINT ||
-      'http://graphql-engine:8080/v1/graphql',
-    secret = process.env.HASURA_GRAPHQL_ADMIN_SECRET
+    endpoint = DEFAULT_HASURA_ENDPOINT,
+    secret = DEFAULT_ADMIN_SECRET
   ) {
     super(
       endpoint,
@@ -21,13 +26,16 @@ class Client extends GraphQLClient {
         : undefined
     )
   }
+
+  /**
+   * @param query GraphQL request, either as string or GraphQL document (e.g. tagged gql`...` request)
+   * @param variables
+   */
   request<T = unknown, V = Variables>(
     query: DocumentNode | string,
     variables?: V
-  ) {
+  ): Promise<T> {
     const strQuery = typeof query === 'string' ? query : print(query)
     return super.request<T, V>(strQuery, variables)
   }
 }
-
-export const hasuraClient = new Client()
