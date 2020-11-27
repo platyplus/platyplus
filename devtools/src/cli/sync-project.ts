dd09bd59024598ced46482b044268f3c897d3a15
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-11-27 14:36:31 +0100

    feat(cli): option --all to sync all projects
    affects: @platyplus/devtools

--- devtools/src/cli/sync-project.ts
+++ devtools/src/cli/sync-project.ts
@@ -6,18 +6,25 @@ import { error } from './error'
 
 type args = {
   project?: string
+  all?: boolean
 }
 
 export const syncProject: CommandModule<args> = {
-  command: 'sync <project>',
+  command: 'sync [project]',
   describe:
     'synchronise project files. Create/update skaffold, and overrides dockerfiles',
   builder: (yargs) =>
-    yargs.positional('project', {
-      describe: 'name of the project to synchronise'
-    }),
+    yargs
+      .positional('project', {
+        describe: 'name of the project to synchronise'
+      })
+      .option('all', {
+        describe: 'sync all the projects of the monorepo',
+        type: 'boolean',
+        default: false
+      }),
   handler: async (options) => {
-    const projects = await listProjects()
+    const projects = await (await listProjects()).map((p) => p.name)
     if (!projects.length)
       throw Error(
         "No project found in the repo. Please create a project first in running 'platy create project <name>'."
@@ -26,13 +33,16 @@ export const syncProject: CommandModule<args> = {
       {
         name: 'project',
         type: 'list',
-        when: !options.project,
-        choices: projects.map((p) => p.name)
+        when: !options.project && !options.all,
+        choices: projects
       }
     ])
     const { project } = { ...options, ...answers }
+    const list = options.all ? projects : [project]
     try {
-      await sync(project)
+      for (const p of list) {
+        await sync(p)
+      }
     } catch (e) {
       error(e)
     }
