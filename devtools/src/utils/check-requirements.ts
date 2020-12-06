bc8accb6eec0055735ff16521806cf8552a122a2
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-12-06 21:46:32 +0100

    perf: add \`git cz\` to recommended installations
    affects: @platyplus/devtools

--- devtools/src/utils/check-requirements.ts
+++ devtools/src/utils/check-requirements.ts
@@ -13,13 +13,18 @@ const dependencies: Dependency[] = [
     version: '19.03.13'
   },
   {
+    bin: 'cz',
+    name: 'git-cz',
+    optional: true
+  },
+  {
     bin: 'helm',
     version: '3.3.4'
   },
   { bin: 'node', version: '14.10.0' },
   { bin: 'skaffold', version: '1.17.0' },
   { bin: 'yarn', version: '1.22.10' },
-  { bin: 'git', version: '2.28.0' },
+  { bin: 'git' },
   { bin: 'quasar', version: '1.1.2', optional: true },
   { bin: 'hasura', name: 'Hasura CLI', version: '1.3.3', optional: true },
   { bin: 'kubectl' }
