dc72904b0d55bce7bd34bb16543b98c326deac14
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2020-12-28 16:09:40 +0100

    perf: getReverseOneToMany. Not used yet but who knows

--- easynut/vue/src/plugins/rxdb-hasura/helpers.ts
+++ easynut/vue/src/plugins/rxdb-hasura/helpers.ts
@@ -1,4 +1,5 @@
-import { CoreTableFragment } from '../../generated'
+import { CoreTableFragment, TableFragment } from '../../generated'
+import { ValuesOf } from '../../utils/helpers'
 
 export const fullTableName = (data: CoreTableFragment): string =>
   data.table_schema === 'public'
@@ -25,3 +26,59 @@ export const warn = (...args: unknown[]): unknown =>
   VERBOSE_LEVEL >= LOG_LEVEL.WARN && console.warn(...args)
 export const error = (...args: unknown[]): unknown =>
   VERBOSE_LEVEL >= LOG_LEVEL.ERROR && console.warn(...args)
+
+/**
+ * * Maps all object relationships of the table, and returns the  array relationships of corresponding remote tables
+ * @param table
+ * @param tables
+ */
+export const getReverseOneToMany = (
+  table: TableFragment,
+  tables: Record<string, TableFragment>
+): Array<{
+  remoteTable: TableFragment
+  from: ValuesOf<TableFragment['relationships']>
+  to: ValuesOf<TableFragment['relationships']>
+}> =>
+  table.relationships
+    .filter(({ rel_type }) => rel_type === 'object')
+    .reduce<
+      Array<{
+        remoteTable: TableFragment
+        from: ValuesOf<TableFragment['relationships']>
+      }>
+    >(
+      (aggr, relation) => [
+        ...aggr,
+        ...Object.values(tables)
+          .filter(curs =>
+            relation.mapping.some(
+              m =>
+                m.remoteTable?.table_name === curs.table_name &&
+                m.remoteTable?.table_schema === curs.table_schema
+            )
+          )
+          .map(table => ({ remoteTable: table, from: relation }))
+      ],
+      []
+    )
+    .map(({ remoteTable, from }) =>
+      remoteTable.relationships
+        .filter(
+          rel =>
+            rel.rel_type === 'array' &&
+            rel.mapping.some(
+              m =>
+                m?.remoteTable?.table_name === table.table_name &&
+                m.remoteTable?.table_schema === table.table_schema
+            )
+        )
+        .map(remote => ({ remoteTable, from, to: remote }))
+    )
+    .reduce<
+      Array<{
+        remoteTable: TableFragment
+        from: ValuesOf<TableFragment['relationships']>
+        to: ValuesOf<TableFragment['relationships']>
+      }>
+    >((aggr, curr) => [...aggr, ...curr], [])
