f14fbf23321b2fcaf5fbd8c565fe085613f1dc57
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-01-05 22:27:12 +0100

    feat: use date and datetime primevue components

--- easynut/vue/src/plugins/vue-rxdb-hasura/components/fields/DateTime.vue
+++ easynut/vue/src/plugins/vue-rxdb-hasura/components/fields/DateTime.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
-div(v-if="editing") editing
+Calendar(v-if="editing" v-model="model" :showTime="true" :showSeconds="true" appendTo="body")
 div(v-else) {{formatedValue}}
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { GenericRxDocument } from '@platyplus/rxdb-hasura'
+import { useFormProperty } from '@platyplus/vue-rxdb-hasura'
+import { computed, defineComponent, PropType, toRefs } from 'vue'
 
 export default defineComponent({
   name: 'FieldDateTime',
   props: {
-    value: {
-      type: String,
-      default: null
+    document: {
+      type: Object as PropType<GenericRxDocument>,
+      required: true
     },
     name: {
       type: String,
@@ -23,8 +25,11 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const formatedValue = computed(() => new Date(props.value).toLocaleString())
-    return { formatedValue }
+    // TODO internationalize preferably without using momentjs (too big)
+    const { name, document } = toRefs(props)
+    const { model } = useFormProperty<string>(document, name)
+    const formatedValue = computed(() => new Date(model.value).toLocaleString())
+    return { formatedValue, model }
   }
 })
 </script>
