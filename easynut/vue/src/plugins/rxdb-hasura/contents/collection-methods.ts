c591cfcabfc2cd81378107bec0012a2ba323ceca
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-01-11 13:04:34 +0100

    feat: permissions on object relationships
    affects: @platyplus/easy-vue

--- easynut/vue/src/plugins/rxdb-hasura/contents/collection-methods.ts
+++ easynut/vue/src/plugins/rxdb-hasura/contents/collection-methods.ts
@@ -2,22 +2,60 @@ import { ContentsCollection, ContentsCollectionMethods } from '../types'
 
 export const collectionMethods: ContentsCollectionMethods = {
   canInsert(this: ContentsCollection, fieldName?: string): boolean {
-    // TODO make it work with relationships
     // ? Check the hasura permission rule ?
-    return (
-      (!!this.metadata.canInsert_aggregate.aggregate?.count && !fieldName) ||
-      !!this.metadata.columns.find(col => col.column_name === fieldName)
-        ?.canInsert.length
-    )
+    if (fieldName) {
+      const property = this.schema.jsonSchema.properties[fieldName]
+      if (property?.ref) {
+        // * Relationship
+        const relationship = this.metadata.relationships.find(
+          ({ rel_name }) => rel_name === fieldName
+        )
+        if (relationship?.rel_type === 'object') {
+          // * object relationship
+          const columns = relationship.mapping.map(m => m.column?.column_name)
+          return columns.every(column => column && this.canInsert(column))
+        } else {
+          // TODO array relationship
+          return false
+        }
+      } else {
+        // * Column
+        const column = this.metadata.columns.find(
+          col => col.column_name === fieldName
+        )
+        return !!column?.canInsert.length
+      }
+    } else {
+      return !!this.metadata.canInsert_aggregate.aggregate?.count
+    }
   },
   canUpdate(this: ContentsCollection, fieldName?: string): boolean {
-    // TODO make it work with relationships
     // ? Check the hasura permission rule ?
-    return (
-      (!!this.metadata.canUpdate_aggregate.aggregate?.count && !fieldName) ||
-      !!this.metadata.columns.find(col => col.column_name === fieldName)
-        ?.canUpdate.length
-    )
+    if (fieldName) {
+      const property = this.schema.jsonSchema.properties[fieldName]
+      if (property?.ref) {
+        // * Relationship
+        const relationship = this.metadata.relationships.find(
+          ({ rel_name }) => rel_name === fieldName
+        )
+        if (relationship?.rel_type === 'object') {
+          // * object relationship
+          const columns = relationship.mapping.map(m => m.column?.column_name)
+          return columns.every(column => column && this.canUpdate(column))
+        } else {
+          // TODO array relationship
+          return false
+        }
+      } else {
+        // * Column
+        const column = this.metadata.columns.find(
+          col => col.column_name === fieldName
+        )
+        return !!column?.canUpdate.length
+      }
+    } else {
+      return !!this.metadata.canUpdate_aggregate.aggregate?.count
+    }
   },
   title(this: ContentsCollection): string {
     return this.metadata.config?.title || this.name
