2d094a8e32b0bfca4ec5ca5d73bab126df60ca74
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-01-12 09:08:31 +0100

    perf: split code into chunks
    affects: @platyplus/vue-rxdb-hasura

--- packages/vue-rxdb-hasura/vue.config.js
+++ packages/vue-rxdb-hasura/vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 200000
+      }
+    }
+  },
   chainWebpack: config => {
     config.module
       .rule('graphql')
