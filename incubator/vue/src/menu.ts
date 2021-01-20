a1fa278fd493d3da5baad5860be040b21caade47
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-01-20 15:37:08 +0100

    fix: show only collections from the default role in the menu
    affects: @platyplus/incubator-vue

--- incubator/vue/src/menu.ts
+++ incubator/vue/src/menu.ts
@@ -10,17 +10,19 @@ export const useMenu = (): ComputedRef<MenuItem[]> => {
     { label: 'Home', icon: 'pi pi-fw pi-home', to: '/home' },
     { label: 'Login', icon: 'pi pi-fw pi-sign-in', to: '/login' },
     { label: 'Register', icon: 'pi pi-fw pi-user-plus', to: '/register' },
-    ...Object.values(collections.value).map(collection => {
-      return {
-        label: collection.title(),
-        icon: `pi pi-fw ${collection.icon() || 'pi-table'}`,
-        to: {
-          name: 'collection',
-          params: {
-            name: collection.name
+    ...Object.values(collections.value)
+      .filter(collection => !collection.role) // * Show only collections from the default role
+      .map(collection => {
+        return {
+          label: collection.title(),
+          icon: `pi pi-fw ${collection.icon() || 'pi-table'}`,
+          to: {
+            name: 'collection',
+            params: {
+              name: collection.name
+            }
           }
         }
-      }
-    })
+      })
   ])
 }
