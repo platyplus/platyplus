60bee2d62db7b84b83e2ae9410685219012f6244
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-01-26 08:54:25 +0100

    fix: correct import
    affects: @platyplus/devtools, @platyplus/amqp, @platyplus/data, @platyplus/fs, @platyplus/gis,
    @platyplus/hasura-client, @platyplus/humanitarian-icons, @platyplus/jsonata-schema,
    @platyplus/lerna, @platyplus/quasar-app-extension-ts-lerna, @platyplus/rxdb-hasura,
    @platyplus/vue-hasura-backend-plus, @platyplus/vue-rxdb-hasura

--- packages/vue-rxdb-hasura/src/composables/profile.ts
+++ packages/vue-rxdb-hasura/src/composables/profile.ts
@@ -1,6 +1,7 @@
 import { ContentsDocument } from '@platyplus/rxdb-hasura'
-import { useSingleton } from '@platyplus/vue-rxdb-hasura'
 import { Ref } from 'vue'
+
+import { useSingleton } from './collection'
 
 export const useProfile = (): Readonly<Ref<ContentsDocument | undefined>> =>
   useSingleton('me_users')
