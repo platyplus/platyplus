b81d198806f9853a2c1edcd52f5003d047fc051b
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-01-31 14:02:02 +0100

    fix: add label to indexes
    affects: @platyplus/rxdb-hasura

--- packages/rxdb-hasura/src/contents/schema/indexes.ts
+++ packages/rxdb-hasura/src/contents/schema/indexes.ts
@@ -1,9 +1,10 @@
 import { Metadata } from '../../types'
 import { propertyJsonType } from './property'
 
-// * Map PostgreSQL indexes
-export const indexes = (table: Metadata): (string | string[])[] =>
-  table.indexes
+// * PostgreSQL indexes + label
+export const indexes = (table: Metadata): (string | string[])[] => {
+  // * Map PostgreSQL indexes
+  const postgresIndexes = table.indexes
     .filter(({ columns }) =>
       columns.every(({ column_name }) => {
         const column = table.columns.find(c => c.column_name === column_name)
@@ -18,3 +19,5 @@ export const indexes = (table: Metadata): (string | string[])[] =>
     .map(({ columns }) =>
       columns.map(({ column_name }) => column_name as string)
     )
+  return [...postgresIndexes, 'label']
+}
