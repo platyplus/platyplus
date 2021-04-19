34830670d24857f6685e1650fdba7d393608b7e8
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-04-19 18:28:33 +0200

    refactor: bump skaffold version
    affects: @platyplus/devtools

--- devtools/src/skaffold/default.ts
+++ devtools/src/skaffold/default.ts
@@ -1,7 +1,7 @@
 import { Skaffold } from './types'
 
 export const defaultSkaffoldConfiguration: Skaffold = {
-  apiVersion: 'skaffold/v2beta8',
+  apiVersion: 'skaffold/v2beta14',
   kind: 'Config',
   build: {
     tagPolicy: {
