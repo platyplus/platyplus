f94b9fc71aef561247635d055b40f7a7dfca706d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-05-24 13:28:46 +0200

    refactor: simplify

--- apps/platyplus/pages/collection/[name]/index.tsx
+++ apps/platyplus/pages/collection/[name]/index.tsx
@@ -1,15 +1,17 @@
-import { ContentsCollection, ContentsDocument } from '@platyplus/rxdb-hasura'
+import { ContentsDocument } from '@platyplus/rxdb-hasura'
 import { useContentsCollection } from '../../../lib/collection'
 import { useRouter } from 'next/router'
 import React, { FunctionComponent, useMemo } from 'react'
 import { useRxQuery } from 'rxdb-hooks'
 
-const CollectionComponent: FunctionComponent<{
-  collection: ContentsCollection
-}> = ({ collection }) => {
-  const query = useMemo(() => collection.find(), [collection])
+const CollectionPage: FunctionComponent = () => {
+  const router = useRouter()
+  const name = router.query.name as string
+  console.log(router.query)
+  const collection = useContentsCollection(name)
+  const query = useMemo(() => collection?.find(), [collection])
   const { isFetching, result } = useRxQuery<ContentsDocument>(query)
-  if (isFetching) return <div>loading contents...</div>
+  if (isFetching) return <div>fetching</div>
   else
     return (
       <div>
@@ -19,14 +21,6 @@ const CollectionComponent: FunctionComponent<{
         ))}
       </div>
     )
-}
-
-const CollectionPage: FunctionComponent = () => {
-  const router = useRouter()
-  const name = router.query.name as string
-  const collection = useContentsCollection(name)
-  if (collection) return <CollectionComponent collection={collection} />
-  else return <div>looking for collection...</div>
 }
 
 export default CollectionPage
