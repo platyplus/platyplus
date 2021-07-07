500a2c730119ad4ce33b55f8bddf9d1eb2e5c04a
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-07-07 16:24:34 +0200

    fix: use rxjs v7 that is installed in the 'root' node_modules instead of the nx/web one (v6)

--- apps/platyplus/webpack.config.js
+++ apps/platyplus/webpack.config.js
@@ -4,6 +4,11 @@ module.exports = (config, context) => {
   nrwlConfig(config) // first call it so that it @nrwl/react plugin adds its configs,
 
   // then override your config.
+  // ! Webpack resolves the rxjs version of Nx (v6) before the one used by RxDB.
+  // ! => Reverse resolve paths so the 'root' node_module goes first...
+  // * See https://github.com/webpack/webpack/issues/6538
+  config.resolve.modules = config.resolve.modules.reverse()
+
   return {
     ...config,
     node: { global: true, fs: 'empty' } // Fix: "Uncaught ReferenceError: global is not defined", and "Can't resolve 'fs'".
