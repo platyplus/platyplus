f793a472eb42a2b1065ce782131f66abb5f37426
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-07-11 11:25:20 +0200

    feat: edit property titles

--- libs/data-access/react-rxdb-hasura/src/metadata/property/title.tsx
+++ libs/data-access/react-rxdb-hasura/src/metadata/property/title.tsx
@@ -4,27 +4,24 @@ import { Icon, Input, InputGroup } from 'rsuite'
 import { useClickAway } from 'react-use'
 import { useCollectionMetadata } from '../hooks'
 import { useConfigStore } from '../store'
-import { useCollectionPropertyConfig } from './hooks'
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
+/** Use the title of a property, in this order:
+ * 1. title stored in the config store
+ * 2. title stored in the metadata property configuration
+ * 3. name of the property
+ * The `setTitle` method will set a new title value to the config store
+ * @return [title, setTitle]
+ */
 export const usePropertyTitle = (
   collection: RxCollection,
   property: string
-) => {
+): [string, (val: string) => void] => {
   const metadata = useCollectionMetadata(collection)
-  const formTitle = useConfigStore(
-    (state) =>
-      metadata &&
-      state.forms[metadata.id]?.propertiesConfig?.find(
-        ({ property_name }) => property_name === property
-      )?.title
-  )
-  const config = useCollectionPropertyConfig(collection, property)
-  const title = formTitle || config?.title || property
-  const setTitle = useConfigStore(
-    (state) => (newTitle) =>
-      state.setPropertyTitle(metadata, property, newTitle)
-  )
+  const title = useConfigStore().getPropertyTitle(metadata, property)
+  const setTitle = useConfigStore((state) => (newTitle: string) => {
+    state.setPropertyTitle(metadata, property, newTitle)
+  })
   return [title, setTitle]
 }
 
@@ -33,50 +30,58 @@ export const PropertyTitle: React.FC<{
   property: string
 }> = ({ collection, property }) => {
   const [title, setTitle] = usePropertyTitle(collection, property)
-  const [editing, edit] = useToggle(false)
+
+  // * Toggle title editing
+  const [editing, toggle] = useToggle(false)
+
+  // * Input value state
+  const [inputValue, setInputValue] = useState('')
+  useEffect(() => {
+    setInputValue(title)
+  }, [title])
+
+  // * Cancel title editing
+  const cancel = () => {
+    toggle(false)
+    setInputValue(title)
+  }
+
+  // * Set title into the config store
+  const validate = () => {
+    toggle(false)
+    setTitle(inputValue)
+  }
+
+  // * If editing and clicking away, stop editing
   const ref = useRef(null)
-  useClickAway(ref, () => {
-    edit(false)
-  })
+  useClickAway(ref, cancel)
+
+  // * Adjust Input width automatically
+  const [width, setWidth] = useState(`${6 + title.length}em`)
+  useEffect(() => setWidth(`${6 + inputValue.length}em`), [inputValue])
 
   return (
     <span>
       {editing ? (
-        <div
-          ref={ref}
-          style={
-            {
-              // minWidth: 100
-            }
-          }
-        >
+        <div ref={ref} style={{ width }}>
           <InputGroup size="xs">
             <Input
               size="xs"
-              onChange={(value) => {
-                console.log(value)
-              }}
+              value={inputValue}
+              onChange={setInputValue}
+              onKeyDown={({ key }) => key === 'Enter' && validate()}
+              autoFocus
             />
-            <InputGroup.Button
-              size="xs"
-              onClick={(value) => {
-                console.log(value)
-              }}
-            >
+            <InputGroup.Button size="xs" onClick={cancel}>
               <Icon icon="close" />
             </InputGroup.Button>
-            <InputGroup.Button
-              size="xs"
-              onClick={(value) => {
-                console.log(value)
-              }}
-            >
+            <InputGroup.Button size="xs" onClick={validate}>
               <Icon icon="check" />
             </InputGroup.Button>
           </InputGroup>
         </div>
       ) : (
-        <span onClick={edit}>{title}</span>
+        <span onClick={toggle}>{title}</span>
       )}
     </span>
   )
