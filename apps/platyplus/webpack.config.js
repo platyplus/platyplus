ab406d45ea80a0f6c870f9065e88ac25a63e33b9
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-08-30 23:45:13 +0200

    refactor: automatic pwa manifest.json

--- apps/platyplus/webpack.config.js
+++ apps/platyplus/webpack.config.js
@@ -1,7 +1,9 @@
-const nrwlConfig = require('@nrwl/react/plugins/webpack.js') // require the main @nrwl/react/plugins/webpack configuration function.
-const { GenerateSW } = require('workbox-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
+const nrwlConfig = require('@nrwl/react/plugins/webpack.js') // require the main @nrwl/react/plugins/webpack configuration function.
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { GenerateSW } = require('workbox-webpack-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+
 module.exports = (config, context) => {
   nrwlConfig(config) // first call it so that it @nrwl/react plugin adds its configs, then override your config.
 
@@ -19,10 +21,29 @@ module.exports = (config, context) => {
         maximumFileSizeToCacheInBytes: 15 * 1024 * 1024 // TODO max 15MB - not ideal at all, find a way to reduce/split main.xxx.es5.js and vendor.js
       })
     )
+
+  // TODO https://github.com/jantimon/favicons-webpack-plugin
   config.plugins.push(
-    new CopyWebpackPlugin({
-      patterns: [{ from: path.resolve(__dirname, './public') }]
-    })
+    ...[
+      new HtmlWebpackPlugin(),
+      new WebpackPwaManifest({
+        name: 'PlatyPlus',
+        short_name: 'PlatyPlus',
+        description: 'PlatyPlus application',
+        display: 'standalone',
+        theme_color: '#066ace',
+        background_color: '#202020',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        ios: true,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve(__dirname, 'src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          }
+        ]
+      })
+    ]
   )
   return {
     ...config,
