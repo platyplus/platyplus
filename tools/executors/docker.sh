42c4b80de3604ad0ae48a12739f4f142fb32265e
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-09-12 13:27:32 +0200

    ci: clean dockerhub description setup

--- tools/executors/docker.sh
+++ tools/executors/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # * docker.sh <build|push> <app-name>
-# TODO only for last commit!!
+# TODO will only for last commit
 
 ORG=platyplus
 WORKING_DIR=apps/$2
@@ -8,7 +8,6 @@ DOCKERFILE=$WORKING_DIR/Dockerfile
 VERSION=$(git describe --tags HEAD | grep charts-$2@ | awk -F @ '{ print v$2 }')
 REPOSITORY=$ORG/$2
 LATEST_TAG=$REPOSITORY:latest
-
 
 case $1 in
   "build") 
@@ -18,21 +17,14 @@ case $1 in
     fi
     ;;
   "push") 
-    echo $TEST_ENV
-    echo $DOCKERHUB_USERNAME
-    echo $DOCKERHUB_PASSWORD
-    echo $TEST_ENV_END
-    # TODO uncomment
-    # docker push --all-tags $REPOSITORY
-    # TODO description from package.json
-    # PACKAGE_VERSION=$(jq -r '.description // "" | select(. != "") // ""' package.json)
-    DESCRIPTION=$2
+    docker push --all-tags $REPOSITORY
+    DESCRIPTION=$(jq -r '.description // "" | select(. != "") // ""' package.json)
     docker run -v $PWD/$WORKING_DIR:/workspace \
       -e DOCKERHUB_USERNAME="$DOCKERHUB_USERNAME" \
       -e DOCKERHUB_PASSWORD="$DOCKERHUB_PASSWORD" \
       -e DOCKERHUB_REPOSITORY=$REPOSITORY \
       -e README_FILEPATH='/workspace/README.md' \
-      -e SHORT_DESCRIPTION=$DESCRIPTION \
+      -e SHORT_DESCRIPTION="$DESCRIPTION" \
       peterevans/dockerhub-description:2
   ;;
    *) echo "unknown command" ;;
