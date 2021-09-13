ef97d00e826c9c4882ec9a3b8b6df5f06d9f50e4
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-09-13 22:16:04 +0200

    docs: change doc site title

--- apps/documentation/docusaurus.config.js
+++ apps/documentation/docusaurus.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'My Site',
+  title: 'Platyplus DevTools',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
--- tools/netlify/skip-build/index.js
+++ tools/netlify/skip-build/index.js
@@ -22,9 +22,5 @@ function projectChanged(currentProject, fromHash, toHash) {
   const output = execSync(getAffected).toString()
   //get the list of changed projects from the output
   const changedProjects = JSON.parse(output).projects
-  if (changedProjects.find((project) => project === currentProject)) {
-    return true
-  } else {
-    return false
-  }
+  return changedProjects.includes(currentProject)
 }
