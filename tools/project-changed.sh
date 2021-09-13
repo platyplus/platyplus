258cd7da84fbeac2c75050083a9061d128c95ff0
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-09-13 21:55:01 +0200

    ci: yarn nx

--- tools/project-changed.sh
+++ tools/project-changed.sh
@@ -3,9 +3,9 @@ PROJECT=$1
 BASE=$2
 HEAD=$3
 
-PROJECTS=$(nx print-affected --base $BASE --head $HEAD --select="projects")
+PROJECTS=$(yarn nx print-affected --base $BASE --head $HEAD --select="projects")
 if [[ ",$PROJECTS," = *",$y,"* ]]; then
-    exit 0
-else
     exit 1
+else
+    exit 0
 fi
