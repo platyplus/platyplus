22b4b3dab61ae867198503bb81e4d22b099b464b
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-10-10 15:51:59 +0200

    ci: correct npm publish script

--- tools/executors/npm-publish.sh
+++ tools/executors/npm-publish.sh
@@ -4,7 +4,7 @@ PACKAGE_NAME=$(jq -r '.name // "" | select(. != "") // ""' $WORKING_DIR/package.
 LAST_VERSION=$(npm view $PACKAGE_NAME .version --silent || echo '0.0.0')
 CURRENT_VERSION=$(jq -r '.version // "" | select(. != "") // ""' $WORKING_DIR/package.json)
 
-if [ "$VAR1" != "$VAR2" ]; then
+if [ "$LAST_VERSION" != "$CURRENT_VERSION" ]; then
     npm publish --access public $WORKING_DIR
 else
     echo "$PACKAGE_NAME@$CURRENT_VERSION has been already published. will do nothing."
