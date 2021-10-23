0c8d540e282065189d71c7dac0226cc2fbce9aad
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-10-23 14:21:52 +0200

    chore: remove old stuff

--- migrate/packages/gis/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './tile-url'
-export * from './xyz-to-quadkey'
-export * from './geojson-to-tiles'
-export * from './lng-lat-zoom-to-xyz'
-export * from './tile-width'
-export * from './nb-tiles-estimation'
