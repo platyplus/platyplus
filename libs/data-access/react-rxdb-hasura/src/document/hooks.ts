4578de53be78c1ec63b154373e2764c208d6237d
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2021-12-02 21:45:23 +0100

    refactor: include global types in every package

--- libs/data-access/react-rxdb-hasura/src/document/hooks.ts
+++ libs/data-access/react-rxdb-hasura/src/document/hooks.ts
@@ -1,59 +1,38 @@
 import { useEffect, useMemo, useState } from 'react'
-import { DeepReadonlyObject } from 'rxdb'
 import { useRxDocument } from 'rxdb-hooks'
 
-import {
-  Contents,
-  ContentsDocument,
-  populateDocument,
-  TableInformation
-} from '@platyplus/rxdb-hasura'
+import { ContentsDocument, TableInformation } from '@platyplus/rxdb-hasura'
 
 import { useContentsCollection } from '../collection/hooks'
 
-export const useDocument = <B extends boolean = false>(
+// TODO probably remove this, as it is only used in 'Pages'
+export const useDocument = (
   tableInfo: TableInformation,
   role: string,
-  id: string,
-  populate?: B
+  id: string
 ): {
-  document: B extends true ? Contents : ContentsDocument
+  document: ContentsDocument
   isFetching: boolean
 } => {
-  type ResultType = DeepReadonlyObject<
-    B extends true ? Contents : ContentsDocument
-  >
   const collection = useContentsCollection(tableInfo, role)
   const queryConstructor = useMemo(() => id !== 'new' && id, [id])
   const { result, isFetching: isDocumentFetching } =
     useRxDocument<ContentsDocument>(collection?.name, queryConstructor)
-  const [document, setDocument] = useState<ResultType>()
+  const [document, setDocument] = useState<ContentsDocument>()
   useEffect(() => {
-    if (id === 'new')
-      setDocument(
-        collection?.newDocument() as DeepReadonlyObject<ContentsDocument>
-      )
+    if (id === 'new') setDocument(collection?.newDocument())
     else if (result) {
       const subscription = result.$.subscribe(async () => {
-        if (populate) {
-          setPopulating(true)
-          const populated = await populateDocument(result)
-          setDocument(populated as ResultType)
-        } else {
-          setDocument(result)
-        }
-        setPopulating(false)
+        setDocument(result as ContentsDocument)
       })
       return () => subscription.unsubscribe()
     }
-  }, [id, result, collection, populate])
-
-  const [isPopulating, setPopulating] = useState(true)
+  }, [id, result, collection])
 
   const isFetching = useMemo(() => {
     if (id === 'new') return !document
-    else return document ? isDocumentFetching || isPopulating : true
-  }, [id, document, isDocumentFetching, isPopulating])
+    else return document ? isDocumentFetching : true
+  }, [id, document, isDocumentFetching])
 
   return { document, isFetching }
 }
